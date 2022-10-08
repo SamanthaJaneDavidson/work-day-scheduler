e0b24406286029804c7923425a599e6afbf7a18b
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-08 15:59:28 -0400

    Added date form and js date

---
+++ script.js
@@ -0,0 +1,19 @@
+//Current date
+var $currentDay = $("#currentDay");
+
+var currentDateTime = moment();
+$currentDay.text(currentDateTime.format("MMM Do, YYYY"));
+
+//Hour blocks 
+
+
+//Current time
+//if else for time
+var currentHour = parseInt(currentDateTime.format("h"));
+console.log(currentHour)
+if(currentHour )
+
+//Input area
+
+
+//Local storage 
