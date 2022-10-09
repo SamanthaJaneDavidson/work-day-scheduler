47a6d5e5c027cf4eb43dd36b0ca182c777cd79d8
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-09 17:29:09 -0400

    Updated still not working local storage

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var task = $(".task");
 var button = $("button");
-var calendarFormEl = $("calendar");
-var hourLabel = $(".label");
-var label = $(".time");
+var hourLabel = $("#label");
+var saveBtn = $("#saveBtn");
+var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
 //Current date
 var $currentDay = $("#currentDay");
@@ -11,70 +11,29 @@ var currentDateTime = moment();
 $currentDay.text(currentDateTime.format("MMM Do, YYYY"));
 
 
-
-//Current time
-// function currentHourLog()
+//Display hour and current time 
 var currentHour = parseInt(currentDateTime.format("h"));
 console.log(currentHour)
 
-// if(currentHour == hourLabel){
-//     $(".task").addClass("present")
-// }
-// else {
-//     if (currentHour < hourLabel)
-//     $(".task").addClass("past")
-// else (currentHour > hourLabel)
-//     $(".task").addClass("future")
-// }
+if(currentHour == hourLabel){
+    $(task).addClass("present")
+}
+else {
+    if (currentHour < hourLabel)
+    $(task).addClass("past")
 
-var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-for (var i = 0; i < times.length; i++) {
-var timesEl = $("time"); 
-timesEl.text(times[i]);
-timesEl.addClass(".hour")
-calendarFormEl.append(timesEl);
-
+    else (currentHour > hourLabel)
+    $(task).addClass("future")
 }
 
 
+//Save tasks
+function saveTasks (){
+    event.preventDefault()
+var taskInput = $("#task").text();
+localStorage.setItem("task", taskInput);
+localStorage.getItem("task");
+}
 
+$("saveBtn").click(saveTasks);
 
-
-
-
-
-
-// var task = JSON.parse(localStorage.getItem$("task"))
-
-// //Save task 
-// function saveTask(){
-
-//     var value = $(".task").val();
-//     task.text(value);
-//     console.log(task);
-
-//     localStorage.setItem("task", text);
-
-// }
-
-// hourFormEl.on("save", saveTask)
-
-// get the text
-// var text = $('#task').text();
-
-// // set the item in localStorage
-// localStorage.setItem('task', text);
-
-// // bind text to 'blur' event for div
-// $('#task').on('blur', function() {
-
-//     // check the new text
-//     var newText = $(this).text();
-
-//     // overwrite the old text
-//     localStorage.setItem('task', newText);
-
-//     // test if it works
-//     alert(localStorage.getItem('task'));
-
-// });
