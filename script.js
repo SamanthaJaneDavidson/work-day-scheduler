8f3586ab3dd7c4167fb9e48982b6dccc58fd2f67
Samantha Davidson <samanthajanedavidson@gmail.com>
2022-10-12 12:03:02 -0400

    updated JS comments

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 var task = $(".task");
 var saveBtn = $(".saveBtn");
 var hourLabel = $(".time");
-var times = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
-//Current date
+//Current date display
 var $currentDay = $("#currentDay");
 
 var currentDateTime = moment();
 $currentDay.text(currentDateTime.format("MMM Do, YYYY"));
 
 
-//Display hour and current time 
+//Display hour with color coding 
 var currentHour = (currentDateTime.hour());
 console.log(currentHour)
 console.log(hourLabel[1].id);
@@ -29,16 +28,12 @@ else if (currentHour > hourLabel[i].id){
     task[i].classList.add("future")}
 }
 
-
-//Save tasks
+//Save tasks to local storage and display tasks upon refresh
 function saveTasks (event){
-    console.log(this.value)
-
     event.preventDefault()
-var taskInput = $(`#input-${this.value}`).val();
-localStorage.setItem(`task-${this.value}`, taskInput);
+    var taskInput = $(`#input-${this.value}`).val();
+    localStorage.setItem(`task-${this.value}`, taskInput);
 }
-
 
 for(i = 9; i < 18; i++){
 $(`#input-${i}`).val(localStorage.getItem(`task-${i}`))
